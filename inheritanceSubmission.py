e82c571645e0f4d5fdcfd55430e35ab7270910e9
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-02-23 18:47:11 -0700

    Create inheritanceSubmission.py

---
+++ inheritanceSubmission.py
@@ -0,0 +1,15 @@
+
+class Vehicle:
+    make = 'NA'
+    model = 'NA'
+    year = 0
+    
+class Motorcycle(Vehicle):
+    offRoad = False
+    streetLegal = False
+
+class Truck(Vehicle):
+    bedLength = "0ft"
+    4WheelDrive = False
+    
+    

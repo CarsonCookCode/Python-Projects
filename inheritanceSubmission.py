900674f8538b77326f98f4a69eeedb8b9466574a
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-02-23 18:48:15 -0700

    Update inheritanceSubmission.py

--- inheritanceSubmission.py
+++ inheritanceSubmission.py
@@ -1,13 +1,15 @@
-
+# Created vehicle class
 class Vehicle:
     make = 'NA'
     model = 'NA'
     year = 0
-    
+
+# Created child class inheriting from Vehicle
 class Motorcycle(Vehicle):
     offRoad = False
     streetLegal = False
 
+# Created child class inheriting from Vehicle
 class Truck(Vehicle):
     bedLength = "0ft"
     4WheelDrive = False
